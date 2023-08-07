45514ab8e61bebff47d94db103d95540c6a96617
sanukumar <sanukumar1138@gmail.com>
2023-08-07 09:09:03 +0530

    updated footer links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,25 @@
+import { footerLinks } from '@/constants';
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
+
+type ColumnProps = {
+  title: string;
+  links: Array<string>;
+};
+
+const FooterColumn = ({ title, links }: ColumnProps) => (
+  <div className='footer_column'>
+    <h4 className='font-semibold'>{title}</h4>
+    <ul className='flex flex-col gap-2 font-normal'>
+      {links.map((link) => (
+        <Link href='/' key={link}>
+          {link}
+        </Link>
+      ))}
+    </ul>
+  </div>
+);
 
 const Footer = () => {
   return (
@@ -11,6 +31,16 @@ const Footer = () => {
             width={115}
             height={38}
             alt='Flexibble'
+          />
+          <p className='text-start text-sm font-normal mt-5 max-w-sx'>
+            Flexibble is the world's leading community for creatives to share,
+            grow, and get hired.
+          </p>
+        </div>
+        <div className='flex flex-wrap gap-12'>
+          <FooterColumn
+            title={footerLinks[0].title}
+            links={footerLinks[0].links}
           />
         </div>
       </div>
