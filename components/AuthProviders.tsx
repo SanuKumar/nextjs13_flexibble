777264c1b870d50a1075b58aee7c956009091a96
sanukumar <sanukumar1138@gmail.com>
2023-08-17 03:02:16 +0530

    added auth provider

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+'use client';
+
+import { useState, useEffect } from 'react';
+import { getProviders, signIn } from 'next-auth/react';
+
+type Providers = {
+  id: string;
+  name: string;
+  type: string;
+  signinUrl: string;
+  callbackUrl: string;
+  signinUrlParams: Record<string, string>;
+};
 
 const AuthProviders = () => {
-  return (
-    <div>AuthProviders</div>
-  )
-}
+  const [providers, setProviders] = useState<Providers | null>(null);
+  return <div>AuthProviders</div>;
+};
 
-export default AuthProviders
+export default AuthProviders;
